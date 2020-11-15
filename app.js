363809acc3baf63611da61a6aa72912c522aef53
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-11-14 19:43:51 -0500

    enum type

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+// using object type to specify what will be in the object
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Django Reinhardt",
+//   age: 110,
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Django Reinhardt",
-    age: 110
+    age: 110,
+    hobbies: ["Guitar", "Music"],
+    role: Role.ADMIN
 };
-console.log(person);
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
+if (person.role === Role.ADMIN) {
+    console.log("is Admin");
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,31 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// using object type to specify what will be in the object
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Django Reinhardt",
+//   age: 110,
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Django Reinhardt",
   age: 110,
+  hobbies: ["Guitar", "Music"],
+  role: Role.ADMIN,
 };
 
-console.log(person);
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is Admin");
+}
